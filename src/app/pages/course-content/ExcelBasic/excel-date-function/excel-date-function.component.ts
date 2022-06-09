11ff3cd38720e61706d82f8aa92e037cced94d96
ritajaghosh <ritajaghosh@gmail.com>
2022-06-09 11:48:26 +0530

    commit

--- src/app/pages/course-content/ExcelBasic/excel-date-function/excel-date-function.component.ts
+++ src/app/pages/course-content/ExcelBasic/excel-date-function/excel-date-function.component.ts
@@ -15,7 +15,7 @@ export class ExcelDateFunctionComponent implements OnInit {
   }
 
   onTabOpen(event: any) {
-    const tabNames: string[] = ['NOW Function', 'Today Function', 'Year Function', 'MONTH Function', 'DATE Function', 'EDATE Function'];
+    const tabNames: string[] = ['NOW Function', 'Today Function', 'Year Function', 'MONTH Function', 'DATE Function', 'EDATE Function', 'EOMONTH Function'];
     this.messageService.add({key: 'myKey1', severity:'info', summary:'VENV Explaining', detail: 'Function: ' + tabNames[event.index]});
   }
 
