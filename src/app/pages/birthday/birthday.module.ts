14b30fac801ec68af377ec2b129481dc0184ef41
sukantahui <huisukanta77@gmail.com>
2022-08-14 01:32:15 +0530

    text scroll added

--- src/app/pages/birthday/birthday.module.ts
+++ src/app/pages/birthday/birthday.module.ts
@@ -6,6 +6,7 @@ import { BirthdayComponent } from './birthday.component';
 import {ButtonModule} from "primeng/button";
 import {GalleriaModule} from "primeng/galleria";
 import {PhotoGalleryModule} from "./photo-gallery/photo-gallery.module";
+import {TextCarouselModule} from "./text-carousel/text-carousel.module";
 
 
 @NgModule({
@@ -18,6 +19,7 @@ import {PhotoGalleryModule} from "./photo-gallery/photo-gallery.module";
     PhotoGalleryModule,
     GalleriaModule,
     ButtonModule,
+    TextCarouselModule,
   ]
 })
 export class BirthdayModule { }
