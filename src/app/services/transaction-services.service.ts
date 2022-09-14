266e79d0dc46df927cf5283ed87a1d948a81a5c8
ngsutradhar <ngsutradhar50@gmail.com>
2022-09-14 19:19:27 +0530

    design changes done 14092022

--- src/app/services/transaction-services.service.ts
+++ src/app/services/transaction-services.service.ts
@@ -120,7 +120,7 @@ export class TransactionServicesService {
   }
 
   fetchFeesChargeDetailsById($id:any){
-    return this.http.get<any>(this.commonService.getAPI() + '/transactions/feesChargedDetails/'+$id)
+    return this.http.get<any>(this.commonService.getAPI() + '/transactions/feesChargedDetailsMain/'+$id)
     .pipe(catchError(this.errorService.serverError), tap(((response: {success: number, data: StudentToCourse[]}) => {
       this.studentToCourseList=response.data;
       console.log("Fees Course Details:",this.studentToCourseList); 
